bd77809e257c8ba919220fd40620d0ce08eb7a69
hencho108 <hendrik.mischo@auto1.com>
2023-04-23 19:43:48 +0200

    update

---
+++ nvim/lua/user/plugins-setup.lua
@@ -0,0 +1,151 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	return
+end
+
+-- Have packer use a popup window
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
+
+-- add list of plugins to install
+return packer.startup(function(use)
+	-- Have packer manage itself
+	use({ "wbthomason/packer.nvim" })
+
+	-- Useful lua functions used by lots of plugins
+	use({ "nvim-lua/plenary.nvim" })
+
+	-- colorscheme
+	use("navarasu/onedark.nvim")
+	-- use({ "lunarvim/colorschemes" })
+	-- use({
+	-- 	"svrana/neosolarized.nvim",
+	-- 	requires = { "tjdevries/colorbuddy.nvim" },
+	-- })
+
+	use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
+
+	use("szw/vim-maximizer") -- maximizes and restores current window
+
+	-- essential plugins
+	use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
+	use("inkarkat/vim-ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
+
+	-- commenting with gc
+	use({ "numToStr/Comment.nvim" })
+
+	-- file explorer
+	use({ "nvim-tree/nvim-tree.lua", commit = "6ad5c26f4d44791699c5538d9773cb141ba033e7" })
+
+	-- use({
+	-- 	"nvim-neo-tree/neo-tree.nvim",
+	-- 	branch = "v2.x",
+	-- 	requires = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+	-- 		"MunifTanjim/nui.nvim",
+	-- 	},
+	-- })
+	-- vs-code like icons
+	use("nvim-tree/nvim-web-devicons")
+
+	-- statusline
+	use("nvim-lualine/lualine.nvim")
+
+	-- fuzzy finding w/ telescope
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+	use({ "nvim-telescope/telescope.nvim" })
+
+	-- autocompletion
+	use("hrsh7th/nvim-cmp") -- completion plugin
+	use("hrsh7th/cmp-buffer") -- source for text in buffer
+	use("hrsh7th/cmp-path") -- source for file system paths
+	use("hrsh7th/cmp-cmdline") -- cmdline completions
+	use({ "saadparwaiz1/cmp_luasnip" }) -- snippet completions
+	use({ "hrsh7th/cmp-nvim-lua" })
+
+	-- Snippets (required by autocompletion
+	use({ "L3MON4D3/LuaSnip" }) --snippet engine
+	use({ "rafamadriz/friendly-snippets" }) -- a bunch of snippets to use
+
+	-- managing and installing lsp servers
+	use({ "williamboman/mason.nvim" }) -- simple to use language server installer
+	use({ "williamboman/mason-lspconfig.nvim" })
+
+	-- configuring lsp servers
+	use({ "neovim/nvim-lspconfig" }) -- enable LSP
+	use({ "hrsh7th/cmp-nvim-lsp" }) -- makes lsp servers appear in autocompletion
+	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+	use({
+		"glepnir/lspsaga.nvim",
+		branch = "main",
+	}) -- enhanced lsp uis
+	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
+
+	-- formatting and linting
+	use({ "jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
+	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+
+	-- treesitter configuration
+	use({ "nvim-treesitter/nvim-treesitter" })
+	use({ "JoosepAlviste/nvim-ts-context-commentstring" })
+
+	-- bufferline
+	use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "nvim-tree/nvim-web-devicons" })
+	use("moll/vim-bbye")
+
+	-- color highlighting
+	use("norcalli/nvim-colorizer.lua")
+
+	-- auto closing
+	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+
+	-- lightening fast navigation
+	use("phaazon/hop.nvim")
+
+	-- Speed up loading Lua modules in Neovim to improve startup time
+	use({ "lewis6991/impatient.nvim" })
+
+	-- guide lines
+	use("lukas-reineke/indent-blankline.nvim")
+
+	-- git integration
+	use({ "lewis6991/gitsigns.nvim" })
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
+end)
