56a95a95c19af254ec305135d1e4fa90530fd4fd
hendrik <46457899+hendrikmi@users.noreply.github.com>
2023-09-17 11:34:22 +0200

    tmux update

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -22,7 +22,7 @@ copy_to_backup() {
 # Call the copy_to_backup function for each dotfile/directory you want to back up,
 # specifying the source file and the target path within the backup directory
 copy_to_backup ".zshrc" "zsh/.zshrc"
-copy_to_backup ".config/nvim" "nvim"
+copy_to_backup ".config/nvim/" "nvim"
 copy_to_backup ".tmux.conf" "tmux/.tmux.conf"
 copy_to_backup ".iterm" "iterm"
 
