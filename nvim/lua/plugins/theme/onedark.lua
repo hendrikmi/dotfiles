a9a22483b47106f89ef2e1f98f77f0c1770545e2
hendrikmi <46457899+hendrikmi@users.noreply.github.com>
2024-01-03 14:27:23 +0100

    Added background image

--- nvim/lua/plugins/theme/onedark.lua
+++ nvim/lua/plugins/theme/onedark.lua
@@ -8,7 +8,7 @@ return {
         local config = {
             -- Main options --
             style = 'dark',               -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-            transparent = false,          -- Show/hide background
+            transparent = true,           -- Show/hide background
             term_colors = true,           -- Change terminal color as per the selected theme style
             ending_tildes = false,        -- Show the end-of-buffer tildes. By default they are hidden
             cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
