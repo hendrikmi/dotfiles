14b5c4107bb45b7b9234e2bcc00e5ccd08426beb
hendrikmi <46457899+hendrikmi@users.noreply.github.com>
2024-04-01 10:42:12 +0200

    MAIN-: Updated Telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -32,8 +32,15 @@ return {
       },
       pickers = {
         find_files = {
+          file_ignore_patterns = { 'node_modules', '.git', '.venv' },
           hidden = true,
         },
+      },
+      live_grep = {
+        file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+        additional_args = function(_)
+          return { '--hidden' }
+        end,
       },
       extensions = {
         ['ui-select'] = {
