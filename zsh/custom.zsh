edfbc48cc6008af9b0cf7ac60dcd77e7d67adb8d
hendrikmi <46457899+hendrikmi@users.noreply.github.com>
2024-07-28 13:59:44 +0200

    Tmux installation notes

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -54,7 +54,7 @@ fh() {
 }
 
 # Tmux
-Always work in a tmux session if Tmux is installed
+# Always work in a tmux session if Tmux is installed
 if which tmux 2>&1 >/dev/null; then
   if [ $TERM != "screen-256color" ] && [  $TERM != "screen" ]; then
     tmux attach -t default || tmux new -s default; exit
