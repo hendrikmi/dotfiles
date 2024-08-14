c03a79379390d9160238241f3d179e5f4b5015ba
hencho108 <46457899+hendrikmi@users.noreply.github.com>
2024-08-14 18:51:31 +0200

    Upgrade to nvim 10

--- homebrew/custom-formulae/neovim.rb
+++ homebrew/custom-formulae/neovim.rb
@@ -3,18 +3,11 @@ class Neovim < Formula
   homepage "https://neovim.io/"
   license "Apache-2.0"
 
+  head "https://github.com/neovim/neovim.git", branch: "master"
+
   stable do
-    url "https://github.com/neovim/neovim/archive/v0.9.2.tar.gz"
-    sha256 "06b8518bad4237a28a67a4fbc16ec32581f35f216b27f4c98347acee7f5fb369"
-
-    # Remove when `mpack` resource is removed.
-    depends_on "luarocks" => :build
-
-    # Remove in 0.10.
-    resource "mpack" do
-      url "https://github.com/libmpack/libmpack-lua/releases/download/1.0.10/libmpack-lua-1.0.10.tar.gz"
-      sha256 "18e202473c9a255f1d2261b019874522a4f1c6b6f989f80da93d7335933e8119"
-    end
+    url "https://github.com/neovim/neovim/archive/refs/tags/v0.10.0.tar.gz"
+    sha256 "372ea2584b0ea2a5a765844d95206bda9e4a57eaa1a2412a9a0726bab750f828"
 
     # Keep resources updated according to:
     # https://github.com/neovim/neovim/blob/v#{version}/cmake.deps/CMakeLists.txt
@@ -22,28 +15,43 @@ class Neovim < Formula
     # TODO: Consider shipping these as separate formulae instead. See discussion at
     #       https://github.com/orgs/Homebrew/discussions/3611
     resource "tree-sitter-c" do
-      url "https://github.com/tree-sitter/tree-sitter-c/archive/v0.20.5.tar.gz"
-      sha256 "694a5408246ee45d535df9df025febecdb50bee764df64a94346b9805a5f349b"
+      url "https://github.com/tree-sitter/tree-sitter-c/archive/refs/tags/v0.21.0.tar.gz"
+      sha256 "6f0f5d1b71cf8ffd8a37fb638c6022fa1245bd630150b538547d52128ce0ea7e"
     end
 
     resource "tree-sitter-lua" do
-      url "https://github.com/MunifTanjim/tree-sitter-lua/archive/v0.0.18.tar.gz"
-      sha256 "659beef871a7fa1d9a02c23f5ebf55019aa3adce6d7f5441947781e128845256"
+      url "https://github.com/tree-sitter-grammars/tree-sitter-lua/archive/refs/tags/v0.1.0.tar.gz"
+      sha256 "230cfcbfa74ed1f7b8149e9a1f34c2efc4c589a71fe0f5dc8560622f8020d722"
     end
 
     resource "tree-sitter-vim" do
-      url "https://github.com/neovim/tree-sitter-vim/archive/v0.3.0.tar.gz"
-      sha256 "403acec3efb7cdb18ff3d68640fc823502a4ffcdfbb71cec3f98aa786c21cbe2"
+      url "https://github.com/tree-sitter-grammars/tree-sitter-vim/archive/refs/tags/v0.4.0.tar.gz"
+      sha256 "9f856f8b4a10ab43348550fa2d3cb2846ae3d8e60f45887200549c051c66f9d5"
     end
 
     resource "tree-sitter-vimdoc" do
-      url "https://github.com/neovim/tree-sitter-vimdoc/archive/v2.0.0.tar.gz"
-      sha256 "1ff8f4afd3a9599dd4c3ce87c155660b078c1229704d1a254433e33794b8f274"
+      url "https://github.com/neovim/tree-sitter-vimdoc/archive/refs/tags/v2.5.1.tar.gz"
+      sha256 "063645096504b21603585507c41c6d8718ff3c11b2150c5bfc31e8f3ee9afea3"
     end
 
     resource "tree-sitter-query" do
-      url "https://github.com/nvim-treesitter/tree-sitter-query/archive/v0.1.0.tar.gz"
-      sha256 "e2b806f80e8bf1c4f4e5a96248393fe6622fc1fc6189d6896d269658f67f914c"
+      url "https://github.com/nvim-treesitter/tree-sitter-query/archive/refs/tags/v0.3.0.tar.gz"
+      sha256 "f878ff37abcb83250e31a6569e997546f3dbab74dcb26683cb2d613f7568cfc0"
+    end
+
+    resource "tree-sitter-python" do
+      url "https://github.com/tree-sitter/tree-sitter-python/archive/refs/tags/v0.21.0.tar.gz"
+      sha256 "720304a603271fa89e4430a14d6a81a023d6d7d1171b1533e49c0ab44f1e1c13"
+    end
+
+    resource "tree-sitter-bash" do
+      url "https://github.com/tree-sitter/tree-sitter-bash/archive/refs/tags/v0.21.0.tar.gz"
+      sha256 "f0515efda839cfede851adb24ac154227fbc0dfb60c6c11595ecfa9087d43ceb"
+    end
+
+    resource "tree-sitter-markdown" do
+      url "https://github.com/tree-sitter-grammars/tree-sitter-markdown/archive/refs/tags/v0.2.3.tar.gz"
+      sha256 "4909d6023643f1afc3ab219585d4035b7403f3a17849782ab803c5f73c8a31d5"
     end
   end
 
@@ -53,32 +61,20 @@ class Neovim < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 arm64_sonoma:   "1233101d31e635e36a2e52206d9720853f5d06c43d11e9a2471b206d0f7534d5"
-    sha256 arm64_ventura:  "1331fb8fbe169fa8df3209f995ac07b1c3d3116b68a56b7d84d5ad2232d19621"
-    sha256 arm64_monterey: "b593f04943f12915e7d3e33a4fb313fa3d9734767a26ed7f7d4ddb9e1bb57346"
-    sha256 arm64_big_sur:  "db136225812aa77d1989562bb21131d7be2764ad099624f3bdfa0700320ab594"
-    sha256 sonoma:         "5dc8434f02f249f349871f1e066de2715f653b16538f6adc6e3dd99a9773848e"
-    sha256 ventura:        "c33d7e0c78d8d3f232b60b34d8203038d66c42cc796bf64d54bc834522805f6c"
-    sha256 monterey:       "000aa80bcbd9d47e0d52d98717a087304ec643b19d15fc5b4b51bf0680b1b988"
-    sha256 big_sur:        "77e2dc10ca228748aa60dbebcac9ea739809f4eabcd4a22f861d5701acd191c6"
-    sha256 x86_64_linux:   "50d91513b35af090a520f5a319bbe5eef6d5ff2d8dbf1e5d7d913e3b9dda3721"
-  end
-
-  # TODO: Replace with single-line `head` when `lpeg`
-  #       is no longer a head-only dependency in 0.10.0.
-  head do
-    url "https://github.com/neovim/neovim.git", branch: "master"
-    depends_on "lpeg"
+    sha256 arm64_sonoma:   "29f56efa4ef3ad9826c6166ae3ff703143038f9b771928cb90927b88bd234e32"
+    sha256 arm64_ventura:  "031b5ec26e73d2523c561bf54ffb9984012f6fd4a8610a41dbf73048713d2060"
+    sha256 arm64_monterey: "5204adbe762b797feb2f8ca3005182eeef43e89bfe753ed8ad8c533cba6805f1"
+    sha256 sonoma:         "2415920449c19c1b50ae5c91e0aff2b54a2c20e10c6bdacfcd77f9f09defce90"
+    sha256 ventura:        "64de1ffb23f9ef9f8f51dd0d33ab19d31a290d33b1d62a422be1d4a4047820f2"
+    sha256 monterey:       "fe5c86b90ee70689f94bfe05ec95f064053ad7223090f64749de8f86b3b8465c"
+    sha256 x86_64_linux:   "77883d08b74050e4a609865c8e113f07b847e6eacc657b9597cf002bbc97395e"
   end
 
   depends_on "cmake" => :build
-  depends_on "lpeg" => :build # needed at runtime in 0.10.0
-  depends_on "pkg-config" => :build
   depends_on "gettext"
-  depends_on "libtermkey"
   depends_on "libuv"
   depends_on "libvterm"
+  depends_on "lpeg"
   depends_on "luajit"
   depends_on "luv"
   depends_on "msgpack"
@@ -98,29 +94,13 @@ class Neovim < Formula
 
     if build.stable?
       cd "deps-build/build/src" do
-        # TODO: Remove `mpack` build block in 0.10.0.
-        cd "mpack" do
-          luajit = Formula["luajit"]
-          lua_path = "--lua-dir=#{luajit.opt_prefix}"
-          deps_build = buildpath/"deps-build"
-
-          # The path separator for `LUA_PATH` and `LUA_CPATH` is `;`.
-          ENV.prepend "LUA_PATH", deps_build/"share/lua/5.1/?.lua", ";"
-          ENV.prepend "LUA_CPATH", deps_build/"lib/lua/5.1/?.so", ";"
-
-          rock = "mpack-1.0.10-0.rockspec"
-          output = Utils.safe_popen_read("luarocks", "unpack", lua_path, rock, "--tree=#{deps_build}")
-          unpack_dir = output.split("\n")[-2]
-
-          cd unpack_dir do
-            system "luarocks", "make", lua_path, "--tree=#{deps_build}"
-          end
-        end
-
         Dir["tree-sitter-*"].each do |ts_dir|
           cd ts_dir do
-            cp buildpath/"cmake.deps/cmake/TreesitterParserCMakeLists.txt", "CMakeLists.txt"
-
+            if ts_dir == "tree-sitter-markdown"
+              cp buildpath/"cmake.deps/cmake/MarkdownParserCMakeLists.txt", "CMakeLists.txt"
+            else
+              cp buildpath/"cmake.deps/cmake/TreesitterParserCMakeLists.txt", "CMakeLists.txt"
+            end
             parser_name = ts_dir[/^tree-sitter-(\w+)$/, 1]
             system "cmake", "-S", ".", "-B", "build", "-DPARSERLANG=#{parser_name}", *std_cmake_args
             system "cmake", "--build", "build"
@@ -141,12 +121,6 @@ class Neovim < Formula
 
     # Replace `-dirty` suffix in `--version` output with `-Homebrew`.
     inreplace "cmake/GenerateVersion.cmake", "--dirty", "--dirty=-Homebrew"
-
-    # Needed to find `lpeg` in non-default prefixes.
-    ENV.prepend "LUA_CPATH", Formula["lpeg"].opt_lib/"lua/5.1/?.so", ";"
-    # Don't clobber the default search path
-    ENV.append "LUA_PATH", ";", ";"
-    ENV.append "LUA_CPATH", ";", ";"
 
     system "cmake", "-S", ".", "-B", "build",
                     "-DLUV_LIBRARY=#{Formula["luv"].opt_lib/shared_library("libluv")}",
