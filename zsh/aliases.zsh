e8dce53084bea310b49df97caa2359a011dee641
Morgan Crouch <mcrouch@atlassian.com>
2024-11-05 11:34:32 +1100

    Fix aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,84 +1,106 @@
-# System
+#!/bin/zsh
+#
+# .aliases - Set whatever shell aliases you want.
+#
+
+# -------------------------------------------------------------------
+# System commands 
+# -------------------------------------------------------------------
 alias shutdown='sudo shutdown now'
 alias restart='sudo reboot'
 alias suspend='sudo pm-suspend'
 alias sleep='pmset sleepnow'
 alias c='clear'
 alias e='exit'
+alias vi='vim' # Mask builtin with better default
 
+# -------------------------------------------------------------------
 # Git
-alias g='git'
+# -------------------------------------------------------------------
 alias ga='git add'
+alias gp='git push'
+alias gl='git log --all --decorate --oneline --graph'
+alias gs='git status'
+alias gss="git status -s"
+alias gd='git diff'
+alias gm='git commit -m'
+alias gma='git commit -am'
+alias gb='git branch'
+alias gc='git checkout'
+alias gcb='git checkout -b'
+alias gra='git remote add'
+alias grr='git remote rm'
+alias gp='git pull'		
+alias gpu='git push'
+# Useful for large repos as only pulls changes for that one branch
+# Instead of a full fetch
+alias gpo='git pull origin'
+alias gcl='git clone'
+alias gf='git fetch'
+alias gbranches='git reflog | grep checkout | cut -d '\'' '\'' -f 8 | awk '\''!seen[$0]++'\'' | head ${1} | cat -n'
+alias glog='git log --graph --oneline --all'
+
+# FZF Commands
 alias gafzf='git ls-files -m -o --exclude-standard | grep -v "__pycache__" | fzf -m --print0 | xargs -0 -o -t git add' # Git add with fzf
 alias grmfzf='git ls-files -m -o --exclude-standard | fzf -m --print0 | xargs -0 -o -t git rm' # Git rm with fzf
 alias grfzf='git diff --name-only | fzf -m --print0 | xargs -0 -o -t git restore' # Git restore with fzf
 alias grsfzf='git diff --name-only | fzf -m --print0 | xargs -0 -o -t git restore --staged' # Git restore --staged with fzf
-alias gf='git fetch'
-alias gs='git status'
-alias gss='git status -s'
-alias gup='git fetch && git rebase'
-alias gtd='git tag --delete'
-alias gtdr='git tag --delete origin'
-alias glo='git pull origin'
-alias gl='git pull'
-alias gb='git branch '
-alias gbr='git branch -r'
-alias gd='git diff'
-alias gco='git checkout '
-alias gcob='git checkout -b '
 alias gcofzf='git branch | fzf | xargs git checkout' # Select branch with fzf
-alias gre='git remote'
-alias gres='git remote show'
-alias glgg='git log --graph --max-count=5 --decorate --pretty="oneline"'
-alias gm='git merge'
-alias gp='git push'
-alias gpo='git push origin'
-alias ggpush='git push origin $(current_branch)'
-alias gc='git commit -v'
-alias gcm='git commit -m'
-alias gcmnv='git commit --no-verify -m'
-alias gcanenv='git commit --amend --no-edit --no-verify'
-# Function to commit with ticket ID from current branch, with optional push
-quick_commit() {
-  local branch_name ticket_id commit_message push_flag
-  branch_name=$(git branch --show-current)
-  ticket_id=$(echo "$branch_name" | awk -F '-' '{print toupper($1"-"$2)}')
-  commit_message="$ticket_id: $*"
-  push_flag=$1
 
-  if [[ "$push_flag" == "push" ]]; then
-    # Remove 'push' from the commit message
-    commit_message="$ticket_id: ${*:2}" # take all positional parameters starting from the second one
-    git commit --no-verify -m "$commit_message" && git push
-  else
-    git commit --no-verify -m "$commit_message"
-  fi
-}
+# -------------------------------------------------------------------
+# Updating built-ins
+# -------------------------------------------------------------------
+eval $(thefuck --alias)
+# Use oxide instead of cd
+alias cd=z
+# Use bat instead of cat
+alias cat=bat
 
-alias gqc='quick_commit'
-alias gqcp='quick_commit push'
+# -------------------------------------------------------------------
+# Alias Commands
+# -------------------------------------------------------------------
+alias del='echo Moving to ~/.Trash/ ...; mv -i $* ~/.Trash/'
+# alias cdf='eval `osascript /Applications/Utilities/OpenTerminal.app/Contents/Resources/Scripts/OpenTerminal.scpt`'
+#alias ls='ls -FG'
+#alias dir='ls -FGl'
+#alias ll="ls -l"
 
-# Neovim
-# If poetry is installed and an environment exists, run "poetry run nvim"
-poetry_run_nvim() {
-  if command -v poetry >/dev/null 2>&1 && [ -f "poetry.lock" ]; then
-    poetry run nvim "$@"
-  else
-    nvim "$@"
-  fi
-}
-alias vi='poetry_run_nvim'
-alias v='poetry_run_nvim'
+# Safe options, this could be dangerous for other apps that aren't expecting these
+alias rm='rm -i'
+alias mv='mv -i'
+alias cp='cp -i'
 
-# Folders
-alias doc="$HOME/Documents"
-alias dow="$HOME/Downloads"
+alias python='python3'
+alias pip='pip3'
 
+# -------------------------------------------------------------------
+# Misc 
+# -------------------------------------------------------------------
 # Ranger
 alias r=". ranger"
 
 # Better ls
 alias ls="eza --all --icons=always"
 
-# Lazygit
-alias lg="lazygit"
+# Misc 
+alias please='sudo'
+alias zshrc='code "${ZDOTDIR:-$HOME}"/.zshrc'
+alias zdot='cd ${ZDOTDIR:-~}'
+
+# fix common typos
+alias quit='exit'
+alias cd..='cd ..'
+
+# -------------------------------------------------------------------
+# Neovim
+# -------------------------------------------------------------------
+# If poetry is installed and an environment exists, run "poetry run nvim"
+# poetry_run_nvim() {
+#   if command -v poetry >/dev/null 2>&1 && [ -f "poetry.lock" ]; then
+#     poetry run nvim "$@"
+#   else
+#     nvim "$@"
+#   fi
+# }
+# alias vi='poetry_run_nvim'
+# alias v='poetry_run_nvim'
