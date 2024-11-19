da41ba78029bcf06a5cff13058da7bbcc66da3e6
Morgan Crouch <mcrouch@atlassian.com>
2024-11-20 09:51:12 +1100

    Remove unused echo statement

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,6 +1,4 @@
-
-echo "imported"
-# fh - search in your command history and execute selected command
+# fh (Find in history) - search in your command history and execute selected command
 fh() {
   eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
 }
