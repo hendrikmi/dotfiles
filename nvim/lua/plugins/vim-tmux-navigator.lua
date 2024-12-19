1e48aa76826a8bd48f540844c73213118ae2fa4a
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2024-12-19 12:03:15 +0200

    Added comment to explain where the vim-tmux config comes from.

--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,3 +1,4 @@
+-- Copied config from https://github.com/christoomey/vim-tmux-navigator
 return {
   'christoomey/vim-tmux-navigator',
   cmd = {
@@ -8,10 +9,10 @@ return {
     'TmuxNavigatePrevious',
   },
   keys = {
-    { '<c-h>',  '<cmd><C-U>TmuxNavigateLeft<cr>' },
-    { '<c-j>',  '<cmd><C-U>TmuxNavigateDown<cr>' },
-    { '<c-k>',  '<cmd><C-U>TmuxNavigateUp<cr>' },
-    { '<c-l>',  '<cmd><C-U>TmuxNavigateRight<cr>' },
+    { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+    { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+    { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+    { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
     { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
   },
 }
