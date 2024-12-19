dfddc7fc66d9054cca0ced8790182ca134f4c269
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2024-12-19 11:43:33 +0200

    Moved vim-tmux-nav config to a separate file (too long for misc imo)

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -1,23 +1,6 @@
 -- Standalone plugins with less than 10 lines of config go here
 return {
   {
-    'christoomey/vim-tmux-navigator',
-    cmd = {
-      'TmuxNavigateLeft',
-      'TmuxNavigateDown',
-      'TmuxNavigateUp',
-      'TmuxNavigateRight',
-      'TmuxNavigatePrevious',
-    },
-    keys = {
-      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
-      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
-      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
-      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
-    },
-  },
-  {
     -- autoclose tags
     'windwp/nvim-ts-autotag',
   },
