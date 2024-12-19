7c0245d65b7656c08cada617ce28bb244286db20
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2024-12-19 11:37:49 +0200

    Added default configuration to vim-tmux-navigator from https://github.com/christoomey/vim-tmux-navigator

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -1,8 +1,21 @@
 -- Standalone plugins with less than 10 lines of config go here
 return {
   {
-    -- tmux & split window navigation
     'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
   },
   {
     -- autoclose tags
