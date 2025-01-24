a323d356c07f1606a765b90aab7674bf8b143b89
hencho108 <46457899+hendrikmi@users.noreply.github.com>
2025-01-24 14:52:01 +0100

    Fix typescript lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -109,7 +109,7 @@ return { -- LSP Configuration & Plugins
     local servers = {
       html = { filetypes = { 'html', 'twig', 'hbs' } },
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      tsserver = {},
+      ts_ls = {},
       lua_ls = {
         -- cmd = {...},
         -- filetypes { ...},
