de6d060b2aede224e9969fa7b5ae5ff7554746a3
hencho108 <46457899+hendrikmi@users.noreply.github.com>
2025-07-15 18:25:46 +0200

    nvim: Fix vim highlight deprecation

--- nvim/lua/core/snippets.lua
+++ nvim/lua/core/snippets.lua
@@ -2,7 +2,7 @@
 
 -- Prevent LSP from overwriting treesitter color settings
 -- https://github.com/NvChad/NvChad/issues/1907
-vim.highlight.priorities.semantic_tokens = 95 -- Or any number lower than 100, treesitter's priority level
+vim.hl.priorities.semantic_tokens = 95 -- Or any number lower than 100, treesitter's priority level
 
 -- Appearance of diagnostics
 vim.diagnostic.config {
@@ -29,7 +29,7 @@ vim.diagnostic.config {
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
   group = highlight_group,
   pattern = '*',
