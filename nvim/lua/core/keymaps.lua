1494f11f543ac17de841517ffe0b86cdc5267814
hendrikmi <46457899+hendrikmi@users.noreply.github.com>
2025-08-02 10:24:30 +0200

    nvim: Fix go forward in jump list

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -46,6 +46,7 @@ vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
 -- Buffers
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
 vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
+vim.keymap.set('n', '<C-i>', '<C-i>', opts)  -- to restore jump forward
 vim.keymap.set('n', '<leader>x', ':Bdelete!<CR>', opts)   -- close buffer
 vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
 
