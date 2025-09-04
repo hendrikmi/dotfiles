5920e5161df0f0264d3f44af959551c70ef0062e
hencho108 <46457899+hendrikmi@users.noreply.github.com>
2025-09-04 09:48:27 +0200

    zsh: comment tmux autostart snippet

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -55,21 +55,21 @@ fh() {
 
 # Tmux
 # Always work in a tmux session if Tmux is installed
-if which tmux >/dev/null 2>&1; then
-  # Check if the current environment is suitable for tmux
-  if [[ -z "$TMUX" && \
-        $TERM != "screen-256color" && \
-        $TERM != "screen" && \
-        -z "$VSCODE_INJECTION" && \
-        -z "$INSIDE_EMACS" && \
-        -z "$EMACS" && \
-        -z "$VIM" && \
-        -z "$INTELLIJ_ENVIRONMENT_READER" ]]; then
-    # Try to attach to the default tmux session, or create a new one if it doesn't exist
-    tmux attach -t default >/dev/null 2>&1 || tmux new -s default
-    exit
-  fi
-fi
+# if which tmux >/dev/null 2>&1; then
+#   # Check if the current environment is suitable for tmux
+#   if [[ -z "$TMUX" && \
+#         $TERM != "screen-256color" && \
+#         $TERM != "screen" && \
+#         -z "$VSCODE_INJECTION" && \
+#         -z "$INSIDE_EMACS" && \
+#         -z "$EMACS" && \
+#         -z "$VIM" && \
+#         -z "$INTELLIJ_ENVIRONMENT_READER" ]]; then
+#     # Try to attach to the default tmux session, or create a new one if it doesn't exist
+#     tmux attach -t default >/dev/null 2>&1 || tmux new -s default
+#     exit
+#   fi
+# fi
 
 # zoxide - a better cd command
 eval "$(zoxide init zsh)"
